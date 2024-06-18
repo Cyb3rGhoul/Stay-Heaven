c25a4a5d0303d35178d2f010fa78ca53c48b8b4e
meetbikhani <bikhanimeet@gmail.com>
2024-06-18 21:38:05 +0530

    updated navbar buttons

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ import profileImage from "./assets/profile.png"; // Replace with the actual path
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const username = "username";
 
   const toggleMenu = () => {
@@ -49,15 +49,24 @@ const Navbar = () => {
             )}
           </li>
         ) : (
-          <div className="flex gap-4">
-            <li>
+          <div className="flex gap-1">
+            <li className="">
               <Link to="/signup" onClick={toggleMenu}>
-                Sign Up
+              <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 max-[500px]:mt-2 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+<span class="relative px-5 py-2.5 max-[500px]:px-2 max-[500px]:py-1.5  transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+Sign Up
+</span>
+</button>
+                
               </Link>
             </li>
             <li>
               <Link to="/login" onClick={toggleMenu}>
-                Login
+              <button class="relative inline-flex max-[500px]:mt-2 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+<span class="relative px-5 py-2.5 max-[500px]:px-2 max-[500px]:py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+Login
+</span>
+</button>
               </Link>
             </li>
           </div>
