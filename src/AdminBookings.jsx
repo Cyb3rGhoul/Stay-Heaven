062d1ccb923a4312818a90335ea8957366a3e8bf
meetbikhani <bikhanimeet@gmail.com>
2024-07-28 17:05:39 +0530

    made admin booking component

--- src/AdminBookings.jsx
+++ src/AdminBookings.jsx
@@ -8,29 +8,49 @@ const AdminBookings = () => {
           <thead>
             <tr className='font-bold text-lg'>
               <th>S.No.</th>
-              <th className='text-center'>Hotel Name</th>
-              <th className='text-center'>Owner</th>
-              <th className='text-center'>Price</th>
-              <th className='text-center'>City</th>
-              <th className='text-center'>State</th>
+              <th className='text-center'>Order Id</th>
+              <th className='text-center'>Hotel Details</th>
+              <th className='text-center'>Check-in Date</th>
+              <th className='text-center'>Check-out Date</th>
+              <th className='text-center'>Rooms</th>
+              <th className='text-center'>Amount</th>
+              <th className='text-center'>Guest Details</th>
               <th className='text-center'>Status</th>
-              <th className='text-center'>Details</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th>1</th>
-              <td className='text-center'>The Grand Hotel</td>
-              <td className='text-center'>John Doe</td>
-              <td className='text-center'>$100</td>
-              <td className='text-center'>New York</td>
-              <td className='text-center'>NY</td>
+              <th className='text-center'>1</th>
+              <td className='text-center'>4355534</td>
               <td className='text-center'>
-                <select className='select bg-zinc-200 select-ghost select-sm'>
-                  <option>Accepted</option>
-                  <option>Rejected</option>
-                </select>
+              <button className="btn bg-zinc-200" onClick={()=>document.getElementById('my_modal_3').showModal()}>More</button>
+                <dialog id="my_modal_3" className="modal">
+                  <div className="modal-box">
+                    <form method="dialog">
+                      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                  </div>
+                </dialog>
               </td>
+              <td className='text-center'>2022-10-01</td>
+              <td className='text-center'>2022-10-05</td>
+              <td className='text-center'>2</td>
+              <td className='text-center'>$1000</td>
+              <td className='text-center'>
+              <button className="btn bg-zinc-200" onClick={()=>document.getElementById('my_modal_3').showModal()}>More</button>
+                <dialog id="my_modal_3" className="modal">
+                  <div className="modal-box">
+                    <form method="dialog">
+                      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                  </div>
+                </dialog>
+              </td>
+              <td className='text-center'>Confirmed</td>
             </tr>
           </tbody>
         </table>
