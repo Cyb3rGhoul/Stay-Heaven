35e047fdc4f4cee6c92d0749768b07dab956be15
Chetan Singh <csingh8299@gmail.com>
2024-11-02 00:39:41 +0530

    fix

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     const [emailOrUsername, setEmailOrUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isLoading, setIsLoading] = useState(false);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const handleError = useHandleErr();
     const dispatch = useDispatch();
     const getUser = async () => {
