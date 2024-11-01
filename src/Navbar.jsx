7ebbb720eccded9e1ce1d427d92e63a7c8c92c78
Chetan Singh <csingh8299@gmail.com>
2024-11-01 14:47:51 +0530

    test fix

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     const [isSticky, setIsSticky] = useState(false);
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(
-        useSelector((state) => state.user.isLoggedIn)
+        useSelector((state) => state.user.userData.isLoggedIn)
     );
     const [profileImage, setprofileImage] = useState(
         useSelector((state) => state.user.userData.avatar)
